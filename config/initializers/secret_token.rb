9c15e74c840fd161201038c47f01d6889d24d855
zme <zme@zme-VirtualBox.(none)>
2013-10-31 12:25:42 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '1a0036c67adac6857fe4af3d1bb89fdc219dc6d079d77eb0dbe646d62e9e26d6416243334d7b910fd5e7a6d29c1e7d7c226c4e7e501de2558aa9c59050c63ba3'
